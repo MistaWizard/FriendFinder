e5653efc3e3386b0a862f2c8003f122910ebc24f
MistaWizard <tylermckellar@gmail.com>
2019-02-07 20:38:40 -0500

    JS Done

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,36 @@
 const friends = [{
-    "name": "Erisdar The Mysterious One",
-    "photo": "",
+    "name": "Moe Szyslak",
+    "photo": "https://upload.wikimedia.org/wikipedia/en/8/80/Moe_Szyslak.png",
+    "scores": [3, 1, 5, 2, 5, 1, 5, 1, 1, 1]
+},
+{
+    "name": "Homer Simpson",
+    "photo": "https://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png",
+    "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+},
+{
+    "name": "Edna Krabappel",
+    "photo": "https://upload.wikimedia.org/wikipedia/en/7/76/Edna_Krabappel.png",
+    "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+},
+{
+    "name": "Ned Flanders",
+    "photo": "https://upload.wikimedia.org/wikipedia/en/8/84/Ned_Flanders.png",
+    "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+},
+{
+    "name": "Krusty The Clown",
+    "photo": "https://upload.wikimedia.org/wikipedia/en/5/5a/Krustytheclown.png",
+    "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+},
+{
+    "name": "Waylon Smithers",
+    "photo": "https://upload.wikimedia.org/wikipedia/en/8/86/Waylon_Smithers_1.png",
+    "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+},
+{
+    "name": "Apu Nahasapeemapetilon",
+    "photo": "https://upload.wikimedia.org/wikipedia/en/2/23/Apu_Nahasapeemapetilon_%28The_Simpsons%29.png",
     "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
 },
 ]
