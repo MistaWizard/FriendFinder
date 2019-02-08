c44e64cddc685e0779f4ac7479e6d04f7d8dac2f
MistaWizard <tylermckellar@gmail.com>
2019-02-07 23:16:57 -0500

    I dunno what I was doing

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,37 +1,37 @@
 const friends = [{
     "name": "Moe Szyslak",
     "photo": "https://upload.wikimedia.org/wikipedia/en/8/80/Moe_Szyslak.png",
-    "scores": [3, 1, 5, 2, 5, 1, 5, 1, 1, 1]
+    "scores": [3, 4, 5, 5, 2, 1, 2, 1, 1, 4]
 },
 {
     "name": "Homer Simpson",
     "photo": "https://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png",
-    "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+    "scores": [3, 5, 4, 1, 1, 3, 5, 1, 4, 2]
 },
 {
     "name": "Edna Krabappel",
     "photo": "https://upload.wikimedia.org/wikipedia/en/7/76/Edna_Krabappel.png",
-    "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+    "scores": [1, 4, 5, 3, 1, 1, 2, 1, 2, 1]
 },
 {
     "name": "Ned Flanders",
     "photo": "https://upload.wikimedia.org/wikipedia/en/8/84/Ned_Flanders.png",
-    "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+    "scores": [5, 1, 2, 5, 5, 5, 1, 5, 5, 5]
 },
 {
     "name": "Krusty The Clown",
     "photo": "https://upload.wikimedia.org/wikipedia/en/5/5a/Krustytheclown.png",
-    "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+    "scores": [2, 3, 5, 2, 3, 4, 4, 3, 3, 3]
 },
 {
     "name": "Waylon Smithers",
     "photo": "https://upload.wikimedia.org/wikipedia/en/8/86/Waylon_Smithers_1.png",
-    "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+    "scores": [4, 1, 2, 1, 5, 1, 2, 5, 3, 4]
 },
 {
     "name": "Apu Nahasapeemapetilon",
     "photo": "https://upload.wikimedia.org/wikipedia/en/2/23/Apu_Nahasapeemapetilon_%28The_Simpsons%29.png",
-    "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+    "scores": [3, 2, 3, 2, 4, 5, 4, 4, 4, 3]
 },
 ]
 
