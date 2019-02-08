0419bb7d76fada24fe709858543d3d7c33ca6fc3
MistaWizard <tylermckellar@gmail.com>
2019-02-07 19:10:54 -0500

    Api done

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 const friends = [{
     "name": "Erisdar The Mysterious One",
-    "photo": "https://forum.unity3d.com/proxy.php?image=http%3A%2F%2Fwww.infinitypbr.com%2Fdragon%2Fhd6.jpg&hash=572c8a1450b0670344e0da08cae18d18",
+    "photo": "",
     "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
 },
 ]
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,10 +1,36 @@
-const friends = require("../data/friends");
+const friends = require("../data/friends.js");
 
 module.exports = function(app) {
     app.get("/api/friends", function(req, res) {
         res.json(friends);
     });
     app.post("/api/friends", function(req, res) {
-        
+        let userData = req.body;
+        let userScore = userData.scores;
+        let totalDifference = 0;
+        let mostCompatible = {
+            name: "",
+            photo: "",
+            differenceBetween: 1000,
+        };
+        for (var i = 0; i < friends.length; i++){
+			//console.log the friend name
+			console.log(friends[i].name);
+			//list variable locally
+			totalDifference = 0;
+			// for loop to go through for scores of same friend data
+			for (var j = 0; j <friends[i].scores[j]; j++){
+				totalDifference += Math.abs(parseInt(userScore[j]) - parseInt(friends[i].scores[j]));
+
+				if(totalDifference <= mostCompatible.differenceBetween){
+					mostCompatible.name = friends[i].name;
+					mostCompatible.photo = friends[i].photo;
+					mostCompatible.differenceBetween = totalDifference;
+				}
+			}
+
+		}
+		friends.push(userData);
+        res.json(mostCompatible);
     })
 }
